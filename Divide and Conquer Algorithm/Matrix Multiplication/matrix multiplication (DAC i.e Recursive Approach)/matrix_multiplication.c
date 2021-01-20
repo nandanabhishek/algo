dcb558849ddc47a0773da71ab7eaa91b2ebc92b3
Abhishek Nandan <abhisheknandan00@gmail.com>
2021-01-20 21:08:06 +0530

    Delete matrix_multiplication.c

--- Divide and Conquer Algorithm/Matrix Multiplication/matrix multiplication (DAC i.e Recursive Approach)/matrix_multiplication.c
+++
@@ -1,5 +0,0 @@
-/*
-When we use a divide-and-conquer algorithm to compute the matrix product C=A*B, we assume that n is an exact power of 2 in each of the n x n matrices.
-We make this assumption because in each divide step, we will divide n x n matrices into four n/2 x n/2 matrices, and by assuming that n is an
-exact power of 2, we are guaranteed that as long as n>=2, the dimension n/2 is an integer.
-*/
