b362aaa7e91f1adf5e3bdd0a24e975d1aa41d622
Abhishek Nandan <abhisheknandan00@gmail.com>
2021-01-20 21:15:17 +0530

    Update strassen_algorithm.c

--- Divide and Conquer Algorithm/Matrix Multiplication/Matrix Multiplication(DAC Approach i.e  Strassen’s algorithm for matrix multiplication)/strassen_algorithm.c
+++ Divide and Conquer Algorithm/Matrix Multiplication/Matrix Multiplication(DAC Approach i.e  Strassen’s algorithm for matrix multiplication)/strassen_algorithm.c
@@ -1,1 +1,6 @@
-.
+/* 
+To keep things simple, when we use a divide-and-conquer algorithm to compute the matrix product C =  A * B, we assume that n is an exact power of 2 in each of the n 	x n matrices. We make this assumption because in each divide step, we will
+divide n x n matrices into four n/2 * n/2 matrices, and by assuming that n is an exact power of 2, we are guaranteed that as long as n >= 2, the dimension n/2 is an
+integer. 
+*/
+
